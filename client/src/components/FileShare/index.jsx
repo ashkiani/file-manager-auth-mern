89ef876b43922f1835fc4f5919d9c8ffd46f7f41
ashkiani <ashkiani@yahoo.com>
2020-05-31 10:08:50 -0400

    Added state to FileShare component

--- client/src/components/FileShare/index.jsx
+++ client/src/components/FileShare/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import FileAccess from "../FileAccess"
 
-export default function index(props) {
+export default function Index(props) {
+    const [shared, setShared] = useState([props.file.shared]);
+    function openClick(e) {
+        console.log("Add/Share Clicked!");
+        console.log(e);
+        console.log(props.file);
+        //props.data.openFunction(e);
+      }
     return (
         <div>
             <input type="text" name="description" placeholder="Enter Username" required />
-            <button>Add</button>
+            <button onClick={() => openClick(props.file.id)}>Add</button>
+            {shared}
             <FileAccess file={props.file}/>
         </div>
     )
